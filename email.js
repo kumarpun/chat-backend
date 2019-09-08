d395832243e2a0dbc43ba892ef6e8b71003c4a8c
kumar-pun <kpun@savadev.com>
2019-09-08 12:22:19 +0545

    server

--- email.js
+++ email.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const multer = require('multer');
 const nodemailer = require("nodemailer");
 const fs = require('fs');
